7fb6c1d441dce6919d7ed1d073c2eb4adaa30935
James King <james.king@facepunchstudios.com>
2024-05-07 09:11:00 +0100

    Fix wall placement, add lights
    https://files.facepunch.com/ziks/2024-05-07/sbox-dev_Vmh62T1kst.mp4

--- code/TestMaze.cs
+++ code/TestMaze.cs
@@ -23,8 +23,11 @@ public sealed class TestMaze : Component
 
 		View = generator.Generate( new MazeGeneratorParameters( Random.Shared.Next(), 16 ) );
 
-		var offset = new Vector3( View.Height, View.Width ) * -24f - Vector3.Up * 192f;
+		var offset = new Vector3( View.Height, View.Width ) * -24f;
+		var vOffset = -Vector3.Up * 192f;
 		var wallModel = Model.Load( "models/wall.vmdl" );
+		var postModel = Model.Load( "models/post.vmdl" );
+		var blockModel = Model.Load( "models/dev/box.vmdl" );
 
 		for ( var j = 0; j <= View.Height; ++j )
 		{
@@ -34,8 +37,9 @@ public sealed class TestMaze : Component
 				{
 					var wall = new GameObject
 					{
+						Name = "Wall",
 						Parent = GameObject,
-						Transform = { Position = new Vector3( j * 48f, i * 48f ) + offset },
+						Transform = { Position = new Vector3( j * 48f, i * 48f + 48f ) + offset + vOffset },
 						Flags = GameObjectFlags.NotNetworked | GameObjectFlags.NotSaved
 					};
 					var renderer = wall.Components.Create<ModelRenderer>();
@@ -51,10 +55,15 @@ public sealed class TestMaze : Component
 			{
 				if ( View[j, i, Direction.West] == WallState.Closed )
 				{
-					var wall = new GameObject {
+					var wall = new GameObject
+					{
+						Name = "Wall",
 						Parent = GameObject,
-						Transform = { Position = new Vector3( j * 48f, i * 48f - 48f ) + offset,
-						Rotation = Rotation.FromYaw( 90f ) },
+						Transform =
+						{
+							Position = new Vector3( j * 48f, i * 48f ) + offset + vOffset,
+							Rotation = Rotation.FromYaw( 90f )
+						},
 						Flags = GameObjectFlags.NotNetworked | GameObjectFlags.NotSaved
 					};
 					var renderer = wall.Components.Create<ModelRenderer>();
@@ -62,6 +71,133 @@ public sealed class TestMaze : Component
 					renderer.Model = wallModel;
 				}
 			}
+		}
+
+		for ( var j = 0; j <= View.Height; ++j )
+		{
+			for ( var i = 0; i <= View.Width; ++i )
+			{
+				var west = View[j, i - 1, Direction.North];
+				var east = View[j, i, Direction.North];
+				var north = View[j - 1, i, Direction.West];
+				var south = View[j, i, Direction.West];
+
+				if ( west == east && north == south )
+				{
+					continue;
+				}
+
+				var post = new GameObject
+				{
+					Name = "Post",
+					Parent = GameObject,
+					Transform = { Position = new Vector3( j * 48f, i * 48f ) + offset + vOffset },
+					Flags = GameObjectFlags.NotNetworked | GameObjectFlags.NotSaved
+				};
+
+				var renderer = post.Components.Create<ModelRenderer>();
+
+				renderer.Model = postModel;
+			}
+		}
+
+		var empty = new List<(int Row, int Col)>();
+		var populated = new HashSet<(int Row, int Col)>();
+
+		for ( var j = -8; j <= View.Height + 4; j += 4 )
+		{
+			for ( var i = -8; i <= View.Width + 4; i += 4 )
+			{
+				var outOfBounds = i < 0 || j < 0 || i >= View.Width || j >= View.Height;
+
+				if ( !outOfBounds )
+				{
+					empty.Clear();
+
+					for ( var l = 0; l < 4; ++l )
+					{
+						for ( var k = 0; k < 4; ++k )
+						{
+							if ( View[j + l, i + k] == CellState.Empty )
+							{
+								empty.Add( (l, k) );
+							}
+						}
+					}
+				}
+
+				if ( outOfBounds || empty.Count == 16 )
+				{
+					var block = new GameObject
+					{
+						Name = "Block",
+						Parent = GameObject,
+						Transform = {
+							Position = new Vector3( (j + 2) * 48f, (i + 2) * 48f ) + offset + new Vector3( 0f, 0f, 30f ),
+							LocalScale = new Vector3( 0.96f * 4f, 0.96f * 4f, 1.2f )
+						}
+					};
+
+					var renderer = block.Components.Create<ModelRenderer>();
+
+					renderer.Model = blockModel;
+					renderer.Tint = Color.Black;
+
+					continue;
+				}
+
+				populated.Add( (j, i) );
+
+				foreach ( var (l, k) in empty )
+				{
+					var block = new GameObject
+					{
+						Name = "Block",
+						Parent = GameObject,
+						Transform = {
+							Position = new Vector3( (j + l) * 48f, (i + k) * 48f ) + offset + new Vector3( 24f, 24f, 30f ),
+							LocalScale = new Vector3( 0.96f, 0.96f, 1.2f )
+						}
+					};
+
+					var renderer = block.Components.Create<ModelRenderer>();
+
+					renderer.Model = blockModel;
+					renderer.Tint = Color.Black;
+				}
+			}
+		}
+
+		var mainHue = Random.Shared.NextSingle() * 360f;
+
+		var lightColors = new[]
+		{
+			new ColorHsv( mainHue, 0.5f, 4f ),
+			new ColorHsv( mainHue - 30f, 0.5f, 4f ),
+			new ColorHsv( mainHue + 30f, 0.5f, 4f )
+		};
+
+		var lightCoords = populated
+			.SelectMany( x => new[]
+			{
+				(x.Row, x.Col), (x.Row - 4, x.Col), (x.Row + 4, x.Col), (x.Row, x.Col - 4), (x.Row, x.Col + 4)
+			} )
+			.Distinct();
+
+		foreach ( var (j, i) in lightCoords )
+		{
+			var light = new GameObject
+			{
+				Name = "Light",
+				Parent = GameObject,
+				Transform = { Position = new Vector3( (j + 2) * 48f, (i + 2) * 48f, 192f ) + offset + Vector3.Random * 48f }
+			};
+
+			var l = light.Components.Create<PointLight>();
+
+			l.LightColor = lightColors[Random.Shared.Next( lightColors.Length )];
+			l.Radius = 48f * 8;
+			l.Shadows = false;
 		}
 	}
 }
